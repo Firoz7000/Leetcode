7135ab15943f7b5a1e5031313cd2de13b73861e6
Firoz Khan <firozbaba0796@gmail.com>
2026-08-05 23:01:21 +0530

    Solutions

---
+++ Patterns/pattern4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    for(int i = 1; i <= n ; i++){
+        int space = i -1;
+        int star = n - i +1;
+        for(int j = 1; j<= space ; j++){
+            cout << " ";
+        }
+        for(int k = 1; k <= star; k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
