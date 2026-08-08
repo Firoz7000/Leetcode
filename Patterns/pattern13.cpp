61adec0a6fd4bf9dd6827b3699eb85398300655b
Firoz Khan <firozbaba0796@gmail.com>
2026-08-08 22:53:50 +0530

    Solution

--- Patterns/pattern13.cpp
+++ Patterns/pattern13.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 int main(){
     int n = 5;
+    
+    // Top half
     for (int i = 1; i <= n; i++){
+        int space = n - i;
+        for (int j = 1; j <= space; j++){
+            cout << " ";
+        }
+        for (int k = 1; k <= i; k++){
+            if (k == 1 || k == i){
+                cout << "*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+    
+    // Bottom half
+    for (int i = n-1; i >= 1; i--){
         int space = n - i;
         for (int j = 1; j <= space; j++){
             cout << " ";
---
+++ Patterns/pattern14.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    for(int i = 1; i<=n; i++){
+        for(int k =1; k<= n; k++){
+            if(i ==1 || i == n || k ==1 || k==n){
+                cout <<"*";
+            }
+            else{
+                cout<< " ";
+            }
+        }
+        cout << endl;
+    }
+}
