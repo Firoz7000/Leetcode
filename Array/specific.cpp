8866040c63261cec6f68bfb20b161b5b08e2e862
Firoz Khan <firozbaba0796@gmail.com>
2026-08-15 22:28:05 +0530

    Array Basics

---
+++ Array/specific.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int target = 30;
+    int index = 0;
+    for(int i = 0; i<n; i++){
+        if(arr[i] == target){
+            index = i;
+            break;
+        }
+    }
+    cout << index;
+}
