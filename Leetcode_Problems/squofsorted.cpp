5948f99574837066fadba23c45907e08a32ad942
Firoz Khan <firozbaba0796@gmail.com>
2026-08-22 22:25:54 +0530

    Leetcode Solution

---
+++ Leetcode_Problems/squofsorted.cpp
@@ -0,0 +1,12 @@
+//977 leetcode Square of sorted array
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> result;
+        for(int i = 0; i < nums.size(); i++){
+            result.push_back(nums[i] * nums[i]);
+        }
+        sort(result.begin() , result.end());
+        return result;
+    }
+};
