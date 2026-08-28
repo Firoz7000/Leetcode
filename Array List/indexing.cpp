b811620a53d7e33594cd55206627e6e6a923010c
Firoz Khan <firozbaba0796@gmail.com>
2026-08-28 23:12:31 +0530

    Array List Solutions

---
+++ Array List/indexing.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {10, 20, 30 ,40, 50 };
+    int n = 5;
+    int element = 30;
+    for(int i = 0; i<n; i++){
+        if(arr[i] == element){
+            cout << i << endl;
+        }
+    }
+}
